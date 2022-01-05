7b3ba2cef961c001c879f4fe7bff673802645571
Azka Miftah Muhammad <azkamiftah29@gmail.com>
2022-01-05 20:15:30 +0700

    membuat projek baru

---
+++ SewaTanah2.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sewatanah2;
+
+/**
+ *
+ * @author Azka Miftah
+ */
+public class SewaTanah2 {
+
+    public double sewa (double luas, double trfpermtr){
+        double HargaSewa;
+        
+        HargaSewa = luas * trfpermtr;
+        return HargaSewa;
+    }
+    
+    public static void main(String[] args) {
+        double hrgsw;
+        SewaTanah2 st = new SewaTanah2();
+        hrgsw = st.sewa(1000, 20);
+        System.out.println(hrgsw);
+    }
+    
+}
